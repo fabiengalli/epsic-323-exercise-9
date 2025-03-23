fe420fbed6e4bf5436638379ca7aeef946e4938d
Fabien <fabien.galli@musewave.ch>
2025-03-23 17:16:18 +0100

    .

--- exercise-9.js
+++ exercise-9.js
@@ -2,7 +2,7 @@
 const queries = [
     "SELECT * FROM pizzas;",
     "SELECT name, size, price FROM pizzas WHERE price > 20 AND price <= 30 ORDER BY price desc LIMIT 5;",
-    "SELECT a.name, b.name as 'category' FROM pizzas a, categories b WHERE a.categoryId=b.id AND b.name IN('Neapolitan', 'Sicilian');"
+    "SELECT a.name, b.name as 'category' FROM pizzas a, categories b WHERE a.categoryId=b.id AND b.name IN ('Neapolitan', 'Sicilian');"
 ];
 
 display(queries);
